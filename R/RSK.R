ef5679f53e3da501685ee185c1f616395a9cc640
stla <laurent_step@yahoo.fr>
2018-07-14 20:39:27 +0200

    RS

---
+++ R/RSK.R
@@ -0,0 +1,33 @@
+bump <- function(P, Q, e, i){
+  if(length(P)==0L) return(list(P=list(e), Q=list(i)))
+  p <- P[[1L]]
+  if(e > p[length(p)]){
+    P[[1L]] <- c(p, e); Q[[1L]] <- c(Q[[1L]], i)
+    return(list(P=P, Q=Q))
+  }else{
+    j <- which.min(p < e)
+    w <- p[j]; P[[1L]][j] <- e
+    b <- bump(P[-1L], Q[-1L], w, i)
+    return(list(P=c(P[1L], b$P), Q=c(Q[1L], b$Q)))
+  }
+}
+
+#' Robinson-Schensted correspondence
+#' @description Pair of standard Young tableaux given from a permutation
+#' by the Robinson-Schensted correspondence.
+#'
+#' @param sigma a permutation given as a vector of integers
+#'
+#' @return A list of two standard Young tableaux.
+#' @export
+#'
+#' @examples
+#' RS(c(1, 3, 6, 4, 7, 5, 2))
+RS <- function(sigma){
+  sigma <- as.integer(sigma)
+  out <- bump(list(), list(), sigma[1L], 1L)
+  for(i in 2L:length(sigma)){
+    out <- bump(out$P, out$Q, sigma[i], i)
+  }
+  return(out)
+}
