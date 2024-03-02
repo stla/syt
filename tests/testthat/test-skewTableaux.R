74be763fdcfbea9a537bf32d8bb128f3d570aaa1
stla <laurent_step@yahoo.fr>
2024-03-02 18:10:03 +0100

    unit tests skew tableaux

--- tests/testthat/test-skewTableaux.R
+++ tests/testthat/test-skewTableaux.R
@@ -1,4 +1,9 @@
 test_that("Skew tableaux", {
   ssstx <- all_ssSkewTableaux(c(4, 2, 1), c(2, 1), 3)
   expect_length(ssstx, 54L)
+  checks <- vapply(ssstx, isSkewTableau, logical(1L))
+  expect_true(all(checks))
+  dssstx <- lapply(ssstx, dualSkewTableau)
+  dchecks <- vapply(dssstx, isSkewTableau, logical(1L))
+  expect_true(all(dchecks))
 })
