3e43312f4fced00e8cead9ef7235afd056fd167b
stla <laurent_step@yahoo.fr>
2024-06-01 18:15:54 +0200

    findGTpatterns

--- inst/GTpatternsMathematica.R
+++ inst/GTpatternsMathematica.R
@@ -26,31 +26,67 @@ findGTpatterns <- function(l, mu, w) {
   restrParts <- lapply(most_w1, function(k) {
     rparts(n-k, ell, m)
   })
-  ip <- lapply(restrParts, function(partitions) {
-    c(list(l), partitions, list(mu))
-  })
-  tuples <- function(i) {
-    Grid <- as.matrix(expand.grid(ip[[i]], ip[[i+1L]]))
-    apply(Grid, 1L, identity, simplify = FALSE)
-  }
+  Grid <- as.matrix(expand.grid(lapply(lengths(restrParts), seq_len)))
+  paths <- apply(Grid, 1L, function(combo) {
+    lapply(seq_len(combo), function(i) {
+      restrParts[[i]][[combo[i]]]
+    })
+  }, simplify = FALSE)
+  # Grid <- cbind(1L, Grid, 1L)
+  pairs <- cbind(
+    head(seq_len(2L + ncol(Grid)), -1L), 
+    tail(seq_len(2L + ncol(Grid)), -1L)
+  )
+  cfs <- apply(Grid, 1L, function(combo) {
+    path <- c(
+      list(l),
+      lapply(seq_len(combo), function(i) {
+        restrParts[[i]][[combo[i]]]
+      }),
+      list(mu)
+    )
+    apply(pairs, 1L, function(ij) {
+      path[ij]
+    }, simplify = FALSE)
+  }, simplify = FALSE)
+  # ip <- lapply(paths, function(path) {
+  #   c(list(l), path, list(mu))
+  # })
+  # tuples <- function(i) {
+  #   Grid <- as.matrix(expand.grid(ip[[i]], ip[[i+1L]]))
+  #   apply(Grid, 1L, identity, simplify = FALSE)
+  # }
   # List <- do.call(
   #   c,
   #   lapply(seq_len(length(ip)-1L), tuples)
   # )
-  List <- lapply(seq_len(length(ip)-1L), tuples)
-  cfs <- unlist(List, recursive = FALSE, use.names = FALSE)
+  # List <- lapply(seq_len(length(ip)-1L), tuples)
+  # cfs <- unlist(List, recursive = FALSE, use.names = FALSE)
+  print("******************")
+  print(str(cfs[[10]]))
+  print("******************")
   condition <- function(cf) {
     min(cf[[1L]] - cf[[2L]]) >= 0 &&
       min(head(cf[[2]], -1L) - tail(cf[[1]], -1L)) >=0
   }
   trues <- which(vapply(cfs, condition, logical(1L)))
-  edgeList <- do.call(rbind, lapply(cfs[trues], function(cf) {
-    cbind(as.character(cf[[1L]]), as.character(cf[[2L]]))
-  }))
+  edgeList <- unique(do.call(rbind, lapply(cfs[trues], function(cf) {
+    c(
+      paste0(as.character(cf[[1L]]), collapse = "-"),
+      paste0(as.character(cf[[2L]]), collapse = "-")
+    )
+    #cbind(as.character(cf[[1L]]), as.character(cf[[2L]]))
+  })))
+  print(vapply(cfs[trues], function(cf) as.integer(sum(cf[[1]]-cf[[2]])), integer(1)))
   print(head(edgeList, 20L))
   gr <- graph_from_edgelist(edgeList)
   print(V(gr))
-  all_simple_paths(gr, from = as.character(l[1L]), to = as.character(mu[1L]))
+  all_simple_paths(
+    gr, 
+    from = paste0(as.character(l), collapse = "-"), 
+    to = paste0(as.character(mu), collapse = "-"), 
+    mode = "total"
+  )
 }
 
 l <- c(4, 3, 3, 2, 1, 1, 0, 0)
